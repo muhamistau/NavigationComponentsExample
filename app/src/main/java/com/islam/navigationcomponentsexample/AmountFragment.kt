52d7afd663492836bfe946b04ef5d156d418060b
muhamistau <islam.taufikurahman@gmail.com>
2019-07-10 14:53:38 +0700

    Initial commit

---
+++ app/src/main/java/com/islam/navigationcomponentsexample/AmountFragment.kt
@@ -0,0 +1,64 @@
+package com.islam.navigationcomponentsexample
+
+
+import android.os.Bundle
+import android.text.TextUtils
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_amount.*
+import java.math.BigDecimal
+
+class AmountFragment : Fragment(), View.OnClickListener {
+
+    lateinit var navController: NavController
+    lateinit var recipient: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        recipient = arguments!!.getString("recipient")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_amount, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        send_btn.setOnClickListener(this)
+        cancel_btn.setOnClickListener(this)
+        recipient_text.text = "Sending money to $recipient"
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id) {
+            R.id.send_btn -> {
+                if (!TextUtils.isEmpty(input_amount.text.toString())) {
+                    val amount = Money(BigDecimal(input_amount.text.toString()))
+                    val bundle = bundleOf(
+                        "recipient" to recipient,
+                        "amount" to amount
+                    )
+                    navController!!.navigate(
+                        R.id.action_amountFragment_to_confirmationFragment,
+                        bundle
+                    )
+                } else {
+                    Toast.makeText(activity, "Enter an amount", Toast.LENGTH_SHORT).show()
+                }
+            }
+            R.id.cancel_btn -> activity!!.onBackPressed()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/islam/navigationcomponentsexample/Money.kt
@@ -0,0 +1,11 @@
+package com.islam.navigationcomponentsexample
+
+import android.annotation.SuppressLint
+import android.os.Parcel
+import android.os.Parcelable
+import kotlinx.android.parcel.Parceler
+import kotlinx.android.parcel.Parcelize
+import java.math.BigDecimal
+
+@Parcelize
+data class Money(val amount: BigDecimal): Parcelable
